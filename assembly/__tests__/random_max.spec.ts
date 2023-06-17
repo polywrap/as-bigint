c8c1bba20837a3bf428d9515e46ad03fe706ca5a
supremestdoggo <83146042+supremestdoggo@users.noreply.github.com>
2023-06-17 12:05:43 -0400

    Lint

--- assembly/__tests__/random_max.spec.ts
+++ assembly/__tests__/random_max.spec.ts
@@ -1,11 +1,11 @@
 import { BigInt } from "../BigInt";
 
 describe("RNG", () => {
-    it("RandomMax", () => {
-        for (var i: i64 = 0; i < 2147483647; i++) {
-            const randomNumber: BigInt = BigInt.randomMax(i);
-            const negRandomNumber: BigInt = BigInt.randomMax(-i);
-            expect(randomNumber.lt(i) && negRandomNumber.gt(i)).toBeTruthy();
-        }
-    })
-})
+  it("RandomMax", () => {
+    for (var i: i64 = 0; i < 2147483647; i++) {
+      const randomNumber: BigInt = BigInt.randomMax(i);
+      const negRandomNumber: BigInt = BigInt.randomMax(-i);
+      expect(randomNumber.lt(i) && negRandomNumber.gt(i)).toBeTruthy();
+    }
+  });
+});
